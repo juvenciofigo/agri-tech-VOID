39c29dac50e5d5fd967d8faff7664c6c170ffedd
juvenciofigo <juvenciofigo@gmail.com>
2025-08-13 10:04:04 +0200

    init project

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
